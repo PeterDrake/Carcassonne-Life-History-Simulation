dabb4bc34209487845dc90c7858f026cd239e107
Dakota Schneider <dakota@hackthetruth.org>
2015-09-08 14:09:55 -0700

    Basic Tile structure

---
+++ src/Model/Tile.java
@@ -0,0 +1,30 @@
+package Model;
+import java.awt.List;
+
+
+public class Tile {
+	
+	// Private members to store tile features
+	// Edges
+	private TileEdge north;
+	private TileEdge east;
+	private TileEdge south;
+	private TileEdge west;
+	// Rotation
+	
+	/**
+	 * Construct a new tile with properties
+	 * 
+	 * @param north
+	 * @param east
+	 * @param south
+	 * @param west
+	 */
+	public void Tile(TileEdge north, TileEdge east, TileEdge south, TileEdge west) {
+		
+	}
+	
+	public TileEdge getNorthFeature() {
+		return null;
+	}
+}
