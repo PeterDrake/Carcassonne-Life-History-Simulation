449e557657920264a87203b2a72447f498aec592
SeniorShizzle <SeniorShizzle@gmail.com>
2015-09-10 12:46:04 -0700

    Added fields for neighbors, including mutators and accessors

--- src/Model/Tile.java
+++ src/Model/Tile.java
@@ -15,16 +15,27 @@ import java.util.Map;
 
 public class Tile {
 
-	// Private members to store tile features
-	// Edges
-	private TileFeature north;
-	private TileFeature east;
-	private TileFeature south;
-	private TileFeature west;
-	// Rotation
+	////// Neighbors /////////
+
+	/** The north neighbor of the tile */
+	private Tile north;
+
+	/** The south neighbor of the tile */
+	private Tile south;
+
+	/** The east neighbor of the tile */
+	private Tile east;
+
+	/** The west neighbor of the tile */
+	private Tile west;
+	
+	////// END NEIGHBORS //////
+
+
+	/**Rotation in degrees*/
 	private Integer rotation;
 	// Image?
-	
+
 	// Features:
 	// - Road End
 	private Boolean roadObstruction;
@@ -32,34 +43,37 @@ public class Tile {
 	private Boolean cityConnects;
 	// - City Shield
 	private Boolean cityShield;
-	
-	
-	/**
-	 * Construct a new tile with features
-	 *
-	 * @param north
-	 * @param east
-	 * @param south
-	 * @param west
-	 */
-	public void Tile(TileFeature north, TileFeature east, TileFeature south, TileFeature west) {
-		this.north = north;
-		this.east = east;
-		this.south = south;
-		this.west = west;
-	}
-	
-	public TileFeature getNorthFeature() {
-		return this.north;
-	}
-	public TileFeature getEastFeature() {
-		return this.east;
-	}
-	public TileFeature getSouthFeature() {
-		return this.south;
-	}
-	public TileFeature getWestFeature() {
-		return this.west;
+
+
+	public Tile getNorth() {
+		return north;
 	}
 
+	public void setNorth(Tile north) {
+		this.north = north;
+	}
+
+	public Tile getSouth() {
+		return south;
+	}
+
+	public void setSouth(Tile south) {
+		this.south = south;
+	}
+
+	public Tile getEast() {
+		return east;
+	}
+
+	public void setEast(Tile east) {
+		this.east = east;
+	}
+
+	public Tile getWest() {
+		return west;
+	}
+
+	public void setWest(Tile west) {
+		this.west = west;
+	}
 }
