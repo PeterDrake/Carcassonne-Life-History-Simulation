d292242a438c3f357ccc7241f3e41209a2890e82
Peter Drake <drake@lclark.edu>
2015-09-15 13:27:15 -0700

    Commented out unimplemented test in DeckTest.

--- src/Model/DeckTest.java
+++ src/Model/DeckTest.java
@@ -11,9 +11,9 @@ public class DeckTest {
 	public void setUp() throws Exception {
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
 
 }
