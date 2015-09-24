3b6dc65ca8e0189568ead5a2874d4c5a361a1d86
SeniorShizzle <SeniorShizzle@gmail.com>
2015-09-24 13:28:20 -0700

    Updated HashMap point tracking utilities

--- src/Model/CarcassonneView.java
+++ src/Model/CarcassonneView.java
@@ -5,6 +5,9 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.swing.*;
 
@@ -12,6 +15,15 @@ import javax.swing.*;
 public class CarcassonneView extends JPanel implements MouseListener, MouseMotionListener{
 
 	public CarcassonneModel game;
+
+    /** a HashMap of Tile and Point objects used for tracking location and mouseEvents */
+    public HashMap<Tile, Point> gameBoard = new HashMap<Tile, Point>();
+
+    /** The nominal width and height of a tile, before scaling */
+    public static final int TILE_WIDTH_NOMINAL = 100;
+
+    /** The scale factor for the tile size */
+    public double scale = 1.0;
 
     /**
      * Instantiates a CarcassonneView object, creates a new Deck, and sets up the principle view window.
@@ -42,12 +54,33 @@ public class CarcassonneView extends JPanel implements MouseListener, MouseMotio
 		Tile startingTile = this.game.startingTile;
 		/** keep track of tiles we've already drawn */
 		ArrayList<Tile> drawnTiles = new ArrayList<Tile>();
-		
+
 		// draw all the tiles, bro
 		drawnTiles = startingTile.drawNeighbors(g2, drawnTiles, Carcassonne.WIDTH/2 - 50, Carcassonne.HEIGHT/2 - 50);
 //		drawnTiles = startingTile.drawNeighbors(g2, drawnTiles, 0, Carcassonne.HEIGHT/2 - 50);
 	}
 
+    public Tile getTileAtPoint(Point point){
+        Iterator<Map.Entry<Tile, Point>> it = this.gameBoard.entrySet().iterator();
+        while (it.hasNext()){
+
+        }
+
+        return null;
+    }
+
+    private boolean pointInTileRect(Point testPoint, Point rectCorner){
+        int dx = rectCorner.x + (int)(TILE_WIDTH_NOMINAL * scale);
+        int dy = rectCorner.y + (int)(TILE_WIDTH_NOMINAL * scale);
+
+        if (testPoint.x >= rectCorner.x && testPoint.x <= dx){
+            if (testPoint.y >= rectCorner.y && testPoint.y <= dy) {
+                return true; // The point is indeed inside the tested rectangle
+            }
+        }
+
+        return false;
+    }
 
 	@Override
 	public void mouseExited(MouseEvent mouseEvent) {
@@ -66,7 +99,8 @@ public class CarcassonneView extends JPanel implements MouseListener, MouseMotio
 
 	@Override
 	public void mouseClicked(MouseEvent mouseEvent) {
-		System.out.println("A Mouse clicked");
+
+        System.out.println("A Mouse clicked at " + mouseEvent.getX() + ", " + mouseEvent.getY());
 	}
 
 	@Override
